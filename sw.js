7f7b42a426b2c6b9cbabcd68c8a63999a46af5f8
miamax_ <mxmmlr@outlook.fr>
2026-08-23 19:09:29 +0200

    chore: cache v17 + manifestes localises

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-const CACHE = 'yam-iamax-v15';
+const CACHE = 'yam-iamax-v17';
 
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './manifest-fr.json',
+  './manifest-en.json',
+  './manifest-de.json',
   './icon.svg',
   './icon-maskable.svg',
 ];
